d4736246ead35a83baa26a2537d370dd6b47e16f
Akash <Akash.kumar@netcorecloud.com>
2022-12-25 20:50:32 +0530

    Auto Layout used in UIView Image and label

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by akash on 23/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
